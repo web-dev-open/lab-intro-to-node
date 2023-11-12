84099738c7b2c0835c05b4ed4bfbd6021472c52b
Soumya <spu.25.nov@gmail.com>
2023-11-12 23:53:41 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,75 @@
+let mocha = require('mocha')
+
 class SortedList {
-  constructor() {}
+  constructor(items,length) {
+     this.items = items;
+     this.length = length;
+  }
 
-  add(item) {}
+  add(item) {
+    
+    this.items.push(item);
 
-  get(pos) {}
+    this.length = this.items.length;
 
-  max() {}
+    this.items.sort((a,b)=>{
+      if(a<b){
+        return -1;
+      }
+      else if(a > b){
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    })
 
-  min() {}
+    return this.items;
+  }
 
-  sum() {}
+  get(pos) {
 
-  avg() {}
+    if(pos<0 && p>=this.items.length){
+      throw new Error('OutOfBounds');
+    }
+    else{
+      return this.items[pos];
+    }
+    
+  }
+
+  max() {
+     if(this.items.length===0){
+      throw new Error('EmptySortedList')
+     }
+     else{
+        return this.items[this.items.length-1];
+     }
+  }
+
+  min() {
+    if(this.items.length===0){
+      throw new Error('EmptySortedList')
+     }
+     else{
+        return this.items[0];
+     }
+  }
+
+  sum() {
+     let sum = this.items.reduce((accum, current)=>{
+      return accum + current
+     })
+     return sum;
+  }
+
+  avg() {
+    let sum = this.items.reduce((accum, current)=>{
+      return accum + current
+     })
+    let average = sum/this.items.length;
+    return average;
+  }
 }
 
 module.exports = SortedList;
