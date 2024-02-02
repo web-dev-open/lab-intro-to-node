899f40ee0440767b2a75de4e64be6e35128e1549
Ragini Nevaskar <ragininevaskar.official7@gmail.com>
2024-02-02 13:47:03 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [],
+    this.length = 0
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length++;
+    this.items.sort((a, b) => {
+      return a - b;
+    })
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= this.length){
+      throw new Error("OutofBounds");
+    }
+    else{
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return this.items[this.length-1];
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.length === 0){
+      throw new Error('EmptySortedList');
+    }
+    else{
+      return this.items[0];
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }
+    let sum = this.items.reduce((acc, current) => {
+      return acc + current;
+    })
 
-  avg() {}
+    return sum;
+  }
+
+  avg() {
+    let sum = this.items.reduce((acc, current) => {
+      return acc + current;
+    })
+    let avg = sum/this.length;
+
+    return avg;
+  }
 }
 
 module.exports = SortedList;
