2409dd1ae7d5ae2d68a4881ba3d75c28dc914112
AdityaKoche01 <adityakoche777@gmail.com>
2024-02-11 13:31:03 +0530

    done

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.length = 0;
+    this.items = [];
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length++;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos <0 || pos >= this.length){
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if(this.length == 0){
+      throw  new Error('EmptySortedList');
 
-  sum() {}
 
-  avg() {}
+    }
+    return this.items[this.length - 1];
+  }
+
+  min() {
+    if(this.length == 0){
+      throw  new Error('EmptySortedList');
+
+
+    }
+    return this.items[0];
+  }
+
+  sum() {
+    if(this.length == 0){
+      return 0;
+
+
+    }
+    return this.items.reduce((a,b) => a + b , 0);
+
+  }
+
+  avg() {
+    if(this.length == 0){
+      throw  new Error('EmptySortedList');
+
+
+    }
+    return this.sum() / this.length;
+
+  }
 }
 
 module.exports = SortedList;
